d3288980149d6462f457c14bc0f05cb3adcbea57
Chung Poon <cmpoon@users.noreply.github.com>
2018-03-13 18:27:29 +0000

    Added leaderbpoard

--- src/container/LeaderBoard/index.js
+++ src/container/LeaderBoard/index.js
@@ -2,21 +2,20 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import LeaderPage from "../../stories/screens/LeaderPage";
-import { fetchList } from "./actions";
+import { leaderBoardData } from "./data";
 
 export interface Props {
 	navigation: any,
 	fetchList: Function,
-	data: Object,
-   graph: Object,
 }
 
 export interface State {}
+
 class LeaderBoard extends React.Component<Props, State> {
 
 	render() {
 		return (
-		<LeaderPage navigation={this.props.navigation} data={this.props.graph}/>
+		<LeaderPage navigation={this.props.navigation} data={leaderBoardData}/>
 		);
 	}
 }
